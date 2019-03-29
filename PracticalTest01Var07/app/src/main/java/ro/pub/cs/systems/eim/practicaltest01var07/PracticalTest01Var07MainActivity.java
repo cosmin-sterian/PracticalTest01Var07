45a7f629dc8430df39fb1dd7721e83841c345584
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2019-03-29 14:50:13 +0200

    secondary activity onCLickListeners

--- PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
+++ PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
@@ -2,12 +2,24 @@ package ro.pub.cs.systems.eim.practicaltest01var07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest01Var07MainActivity extends AppCompatActivity {
+
+	EditText editText1, editText2, editText3, editText4;
+	Button button_set;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_practical_test01_var07_main);
+
+		editText1 = findViewById(R.id.editText1);
+		editText2 = findViewById(R.id.editText2);
+		editText3 = findViewById(R.id.editText3);
+		editText4 = findViewById(R.id.editText4);
+
+		button_set = findViewById(R.id.button_set);
 	}
 }
--- PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07SecondaryActivity.java
+++ PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07SecondaryActivity.java
@@ -2,12 +2,59 @@ package ro.pub.cs.systems.eim.practicaltest01var07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PracticalTest01Var07SecondaryActivity extends AppCompatActivity {
+
+	private EditText editText1, editText2, editText3, editText4;
+	private Button button_sum, button_product;
+	private MathClickListener mathClickListener = new MathClickListener();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_practical_test01_var07_secondary);
+
+		editText1 = findViewById(R.id.editText1);
+		editText2 = findViewById(R.id.editText2);
+		editText3 = findViewById(R.id.editText3);
+		editText4 = findViewById(R.id.editText4);
+
+		button_sum = findViewById(R.id.button_sum);
+		button_product = findViewById(R.id.button_product);
+		button_sum.setOnClickListener(mathClickListener);
+		button_product.setOnClickListener(mathClickListener);
+	}
+
+	class MathClickListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			int result;
+			switch (v.getId()) {
+				case R.id.button_sum:
+					result = Integer.parseInt(editText1.getText().toString()) +
+							Integer.parseInt(editText2.getText().toString()) +
+							Integer.parseInt(editText3.getText().toString()) +
+							Integer.parseInt(editText4.getText().toString());
+					break;
+
+				case R.id.button_product:
+					result = Integer.parseInt(editText1.getText().toString()) *
+							Integer.parseInt(editText2.getText().toString()) *
+							Integer.parseInt(editText3.getText().toString()) *
+							Integer.parseInt(editText4.getText().toString());
+					break;
+
+				default:
+					Log.w("PT01V07SecAct", "another view got in the MathClickListener, aborting");
+					return;
+			}
+			Toast.makeText(PracticalTest01Var07SecondaryActivity.this, "Result is: " + result, Toast.LENGTH_SHORT).show();
+		}
 	}
 }
